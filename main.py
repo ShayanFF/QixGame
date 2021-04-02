220037b67f057e2d43aefb842efdb3c1ce34ec71
Shayan <shayan.firouzian@ryerson.ca>
2021-04-02 13:14:49 -0400

    Updated Qix.move()

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ pygame.draw.line(screen, RED, (25, 25), (25, 575), 5)
 pygame.draw.line(screen, RED, (25, 780), (580, 780), 5)
 pygame.draw.line(screen, RED, (20, 20), (780, 20), 5)"""
 
-
 running = True
 while running:
     for event in pygame.event.get():
@@ -39,9 +38,13 @@ class Player:
     def __init__(self, life, speed):
         self.life = life
         self.speed = speed
+        self.x = 0
+        self.y = 0
 
     # This one will be able to control it's own movement
-    def move(self):
+    def move(self, moveX, moveY):
+        self.x += moveX
+        self.y += moveY
 
     # This allows it to create a path
     def push(self):
@@ -49,11 +52,33 @@ class Player:
 class Qix(Player):
     def __init__(self, life, speed, damage):
         super().__init__(life, speed)
+        self.x = 0
+        self.y = 0
+        self.direction = "Right"
         self.damage = damage
     
     # This one will move in a circle
-    def move(self):
-
+    def move(self, boardX, boardY):
+        if self.direction == "Up:
+            if self.y += (10 * self.speed) >= boardY:
+                self.y = boardY
+            else:
+                self.y += 10 * self.speed
+        elif self.direction == "Down":
+            if self.y -= (10 * self.speed) <= 0:
+                self.y = 0
+            else:
+                self.y -= 10 * self.speed
+        elif self.direction == "Left":
+            if self.x -= (10 * self.speed) <= 0:
+                self.x = 0
+            else:
+                self.x -= 10 * self.speed
+        else:
+            if self.x += (10 * self.speed) >= boardX:
+                self.x = boardX
+            else:
+                self.x += 10 * self.speed
 
 class Sparx(Qix):
     # This one will be able to move on other lines, so overriding other method
