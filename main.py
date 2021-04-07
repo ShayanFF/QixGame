b1092d949ad47ae7a3737d61fb09936cc930a441
HudsonPryde <68714277+HudsonPryde@users.noreply.github.com>
2021-04-06 21:52:26 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from pygame.locals import *
 
 pygame.init()
@@ -6,18 +7,16 @@ pygame.init()
 """Sets size of window, can be changed later"""
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Qix')
-clock = pygame.time.Clock()
 
 """Some placeholder colours to be used later"""
-BLACK = (0, 0, 0)
+BLACK = (0,   0,   0)
 WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+RED = (255,   0,   0)
+GREEN = (0, 255,   0)
+BLUE = (0,   0, 255)
 AQUA = (0, 255, 255)
 
-screen.fill(AQUA)
-
+screen.fill(BLACK)
 """
 You can increment these with maxWidth and height to draw the original border
 Replace:
@@ -25,59 +24,31 @@ Replace:
 575 with (maxWidth - 25)
 and 25 should be able to stay the same although I haven't tested it yet
 """
-playerSurf = pygame.Surface((800, 600))
-playerSurf.fill(AQUA)
-pygame.draw.line(screen, RED, (775, 25), (775, 575), 1)
-pygame.draw.line(screen, RED, (25, 25), (25, 575), 1)
-pygame.draw.line(screen, RED, (25, 575), (775, 575), 1)
-pygame.draw.line(screen, RED, (25, 25), (775, 25), 1)
-pygame.draw.line(playerSurf, RED, (775, 25), (775, 575), 1)
-pygame.draw.line(playerSurf, RED, (25, 25), (25, 575), 1)
-pygame.draw.line(playerSurf, RED, (25, 575), (775, 575), 1)
-pygame.draw.line(playerSurf, RED, (25, 25), (775, 25), 1)
-pygame.draw.rect(screen, BLACK, (400, 570, 10, 10))
-x = 400
-y = 570
-speed = 5
+# pygame.draw.line(screen, RED, (775, 25), (775, 575), 1)
+# pygame.draw.line(screen, RED, (25, 25), (25, 575), 1)
+# pygame.draw.line(screen, RED, (25, 575), (775, 575), 1)
+# pygame.draw.line(screen, RED, (25, 25), (775, 25), 1)
 
 
-def player():
-    pygame.draw.rect(screen, BLACK, (x, y, 10, 10))
 
-
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            running = False
-            pygame.quit()
-            exit()
-
-    keys = pygame.key.get_pressed()
-
-    if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and x > 20:
-        x -= speed
-    elif (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and x < 770:
-        x += speed
-    elif (keys[pygame.K_UP] or keys[pygame.K_w]) and y > 20:
-        y -= speed
-    elif (keys[pygame.K_DOWN] or keys[pygame.K_s]) and y < 570:
-        y += speed
-
-    '''screen = pygame.Surface.copy(screen)'''
-    clock.tick(30)
-    screen.fill(AQUA)
-    screen.blit(playerSurf, (0, 0))
-    player()
-    pygame.display.update()
 
 
 # Board Object
 class Board:
     def __init__(self, start, size):
-        self.maxHeight, self.maxWidth = start + size
-        self.minHeight, self.minWidth = start
-
+        self.maxHeight = start + size
+        self.maxWidth = start + size
+        self.minHeight = start
+        self.minWidth = start
+        self.boarder = []
+        for i in range(start, size, 5):
+            self.boarder.append((i, start))
+        for i in range(start, size, 5):
+            self.boarder.append((self.maxWidth, i))
+        for i in range(self.maxHeight, start, -5):
+            self.boarder.append((i, self.maxHeight))
+        for i in range(self.maxHeight, start, -5):
+            self.boarder.append((start, i))
     def getMaxWidth(self):
         return self.maxWidth
     def getMaxHeight(self):
@@ -86,6 +57,12 @@ class Board:
         return self.minWidth
     def getMinHeight(self):
         return self.minHeight
+    def drawBoard(self):
+        pygame.draw.polygon(screen, RED, self.boarder, 1)
+    def getCoord(self, n):
+        return self.boarder[n]
+
+
 
 class Player:
     def __init__(self, life, speed):
@@ -100,6 +77,7 @@ class Player:
         self.y += moveY
 
     # This allows it to create a path
+    '''
     def push(self, board):
         if self.x == board.getMinWidth():
             
@@ -110,6 +88,7 @@ class Player:
         elif self.y == board.getMinHeight():
         
         elif self.y == board.getMaxHeight():
+    '''
 
 class Qix(Player):
     def __init__(self, life, speed, damage):
@@ -121,28 +100,67 @@ class Qix(Player):
     
     # This one will move in a circle
     def move(self, board):
-        if self.direction == "Up:
-            if self.y += (10 * self.speed) >= board.getMaxHeight():
+        if self.direction == "Up":
+            if self.y + (10 * self.speed) >= board.getMaxHeight():
                 self.y = board.getMaxHeight()
             else:
                 self.y += 10 * self.speed
         elif self.direction == "Down":
-            if self.y -= (10 * self.speed) <= board.getMinHeight():
+            if self.y - (10 * self.speed) <= board.getMinHeight():
                 self.y = board.getMinHeight()
             else:
                 self.y -= 10 * self.speed
         elif self.direction == "Left":
-            if self.x -= (10 * self.speed) <= board.getMinWidth():
+            if self.x - (10 * self.speed) <= board.getMinWidth():
                 self.x = board.getMinWidth()
             else:
                 self.x -= 10 * self.speed
         else:
-            if self.x += (10 * self.speed) >= board.getMaxWidth():
+            if self.x + (10 * self.speed) >= board.getMaxWidth():
                 self.x = board.getMaxWidth()
             else:
                 self.x += 10 * self.speed
 
 # Will fix this later
-'''class Sparx(Qix):
+class Sparx(Qix):
+    def __init__(self, life, speed, damage):
+        super().__init__(life, speed, damage)
+        # pos represents the item in the board coords list that the sparx is currently at
+        self.pos = 10
+        self.x = 0
+        self.y = 0
     # This one will be able to move on other lines, so overriding other method
-    def move(self):'''
+    def move(self, board):
+        newpos = board.getCoord(self.pos)
+        self.x = newpos[0]
+        self.y = newpos[1]
+        # use ints if updating speed
+        if self.pos + self.speed < len(board.boarder):
+            self.pos += self.speed
+        else:
+            self.pos = 0
+    def draw(self):
+        # draw a diamond at the sparx location
+        points = [(self.x+5, self.y),(self.x, self.y+5),(self.x-5, self.y),(self.x, self.y-5)]
+        pygame.draw.polygon(screen, GREEN, points, 1)
+        
+
+# initialize board object
+board = Board(5, 500)
+# initialize sparx object
+sparx = Sparx(4, 1, 1)
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+            pygame.quit()
+            sys.exit()
+    # draw the board
+    board.drawBoard()
+    sparx.move(board)
+    sparx.draw()
+    pygame.display.update()
+    screen.fill(BLACK)
