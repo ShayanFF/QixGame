a2f33e29aed82af7f37c8854fc2edab054f56d7c
Shayan <shayanf1001@gmail.com>
2021-04-07 23:49:51 -0400

    Added Board and Player

--- main.py
+++ main.py
@@ -34,104 +34,19 @@ and 25 should be able to stay the same although I haven't tested it yet
 # pygame.draw.line(screen, RED, (25, 575), (775, 575), 1)
 # pygame.draw.line(screen, RED, (25, 25), (775, 25), 1)
 
-
-# Board Object
-class Board:
-    def __init__(self, start, size):
-        self.maxHeight = start + size
-        self.maxWidth = start + size
-        self.minHeight = start
-        self.minWidth = start
-        self.boarder = []
-        for i in range(start, size, 5):
-            self.boarder.append((i, start))
-        for i in range(start, size, 5):
-            self.boarder.append((self.maxWidth, i))
-        for i in range(self.maxHeight, start, -5):
-            self.boarder.append((i, self.maxHeight))
-        for i in range(self.maxHeight, start, -5):
-            self.boarder.append((start, i))
-
-    def getMaxWidth(self):
-        return self.maxWidth
-
-    def getMaxHeight(self):
-        return self.maxHeight
-
-    def getMinWidth(self):
-        return self.minWidth
-
-    def getMinHeight(self):
-        return self.minHeight
-
-    def drawBoard(self):
-        pygame.draw.polygon(screen, RED, self.boarder, 1)
-        pygame.draw.polygon(playerSurf, RED, self.boarder, 1)
-
-    def getCoord(self, n):
-        return self.boarder[n]
-
-
-class Player:
-    def __init__(self, life, speed):
-        self.life = life
-        self.speed = speed
-        self.x = 0
-        self.y = 0
-
-    # This one will be able to control it's own movement
-    def move(self, moveX, moveY):
-        self.x += moveX
-        self.y += moveY
-
-    # This allows it to create a path
-    '''
-    def push(self, board):
-        if self.x == board.getMinWidth():
-
-
-        elif self.x == board.getMaxWidth():
-
-
-        elif self.y == board.getMinHeight():
-
-        elif self.y == board.getMaxHeight():
-    '''
-
-
+'''
 class Qix(Player):
     def __init__(self, life, speed, damage):
         super().__init__(life, speed)
-        self.x = 0
-        self.y = 0
-        self.direction = "Right"
+        self.x = 10
+        self.y = 10
         self.damage = damage
 
     # This one will move in a circle
-    def move(self, board):
-        if self.direction == "Up":
-            if self.y + (10 * self.speed) >= board.getMaxHeight():
-                self.y = board.getMaxHeight()
-            else:
-                self.y += 10 * self.speed
-        elif self.direction == "Down":
-            if self.y - (10 * self.speed) <= board.getMinHeight():
-                self.y = board.getMinHeight()
-            else:
-                self.y -= 10 * self.speed
-        elif self.direction == "Left":
-            if self.x - (10 * self.speed) <= board.getMinWidth():
-                self.x = board.getMinWidth()
-            else:
-                self.x -= 10 * self.speed
-        else:
-            if self.x + (10 * self.speed) >= board.getMaxWidth():
-                self.x = board.getMaxWidth()
-            else:
-                self.x += 10 * self.speed
-
+    #def move(self, board):   
 
 # Will fix this later
+
 class Sparx(Qix):
     def __init__(self, life, speed, damage):
         super().__init__(life, speed, damage)
@@ -155,13 +70,13 @@ class Sparx(Qix):
         # draw a diamond at the sparx location
         points = [(self.x + 5, self.y), (self.x, self.y + 5), (self.x - 5, self.y), (self.x, self.y - 5)]
         pygame.draw.polygon(screen, BLUE, points, 0)
-        '''pygame.draw.polygon(playerSurf, GREEN, points, 1)'''
+        pygame.draw.polygon(playerSurf, GREEN, points, 1)
 
 
 # initialize board object
 board = Board(5, 500)
 # initialize sparx object
-sparx = Sparx(4, 1, 1)
+sparx = Sparx(4, 1, 1)'''
 
 pygame.draw.rect(screen, BLACK, (255, 505, 10, 10))
 x = 255
@@ -170,19 +85,219 @@ speed = 5
 running = True
 direction = None
 
-def player(direction):
-    for i in range (0,5):
-        if direction == 0:
-            playerSurf.set_at((x, y-i), RED)
-        elif direction == 1:
-            playerSurf.set_at((x-i, y), RED)
-        elif direction == 3:
-            playerSurf.set_at((x, y+i), RED)
-        elif direction == 4:
-            playerSurf.set_at((x+i, y), RED)
-    pygame.draw.rect(screen, BLACK, (x-5, y-5, 10, 10))
+UP = 1
+DOWN = -1
+LEFT = 2
+RIGHT = -2
 
+class Player:
+    def __init__(self, life, speed, board):
+        self.life = life
+        self.speed = speed
+        self.x = 100
+        self.y = 10
+        self.location = board.curr
+        self.atCorner = False
 
+    def move(self, direction, board):
+        if self.atCorner is False:
+            if direction == LEFT and (self.location.orientation == LEFT or self.location.orientation == RIGHT):
+                if self.location.orientation == LEFT and self.x - self.speed <= self.location.next.x:
+                    self.x = self.location.next.x
+                    self.atCorner = True
+                
+                elif self.location.orientation == RIGHT and self.x - self.speed <= self.location.x:
+                    self.x = self.location.x
+                    self.atCorner = True
+
+                else:
+                    self.x -= self.speed
+
+            elif direction == RIGHT and (self.location.orientation == LEFT or self.location.orientation == RIGHT):
+                if self.location.orientation == LEFT and self.x + self.speed >= self.location.x:
+                    self.x = self.location.x
+                    self.atCorner = True
+                
+                elif self.location.orientation == RIGHT and self.x + self.speed >= self.location.next.x:
+                    self.x = self.location.next.x
+                    self.atCorner = True
+
+                else:
+                    self.x += self.speed
+
+            elif direction == UP and (self.location.orientation == UP or self.location.orientation == DOWN):
+                if self.location.orientation == UP and self.y + self.speed >= self.location.next.y:
+                    self.y = self.location.next.y
+                    self.atCorner = True
+                
+                elif self.location.orientation == DOWN and self.y + self.speed >= self.location.y:
+                    self.y = self.location.y
+                    self.atCorner = True
+
+                else:
+                    self.y += self.speed
+
+            elif direction == DOWN and (self.location.orientation == UP or self.location.orientation == DOWN):
+                if self.location.orientation == UP and self.y - self.speed <= self.location.y:
+                    self.y = self.location.y
+                    self.atCorner = True
+                
+                elif self.location.orientation == DOWN and self.y - self.speed <= self.location.next.y:
+                    self.y = self.location.next.y
+                    self.atCorner = True
+
+                else:
+                    self.y -= self.speed
+        else:
+            if direction == UP:
+                if self.location.next.x == self.x and self.location.next.orientation == UP:
+                    self.location = self.location.next
+                    board.curr = board.curr.next
+                    self.y += self.speed
+                    self.atCorner = False
+                elif self.location.prev.x == self.x and self.location.prev.orientation == DOWN:
+                    self.location = self.location.prev
+                    self.y += self.speed
+                    board.curr = board.curr.prev
+                    self.atCorner = False
+
+            elif direction == DOWN:
+                if self.location.next.x == self.x and self.location.next.orientation == DOWN:
+                    self.location = self.location.next
+                    self.y -= self.speed
+                    board.curr = board.curr.next
+                    self.atCorner = False
+                elif self.location.prev.x == self.x and self.location.prev.orientation == UP:
+                    self.location = self.location.prev
+                    self.y -= self.speed
+                    board.curr = board.curr.prev
+                    self.atCorner = False
+                
+            elif direction == LEFT:
+                if self.location.next.y == self.y and self.location.next.orientation == LEFT:
+                    self.location = self.location.next
+                    self.x -= self.speed
+                    board.curr = board.curr.next
+                    self.atCorner = False
+                elif self.location.prev.y == self.y and self.location.prev.orientation == RIGHT:
+                    self.location = self.location.prev
+                    self.x -= self.speed
+                    board.curr = board.curr.prev
+                    self.atCorner = False
+
+            elif direction == RIGHT:
+                if self.location.next.y == self.y and self.location.next.orientation == RIGHT:
+                    self.location = self.location.next
+                    self.x += self.speed
+                    board.curr = board.curr.next
+                    self.atCorner = False
+                elif self.location.prev.y == self.y and self.location.prev.orientation == LEFT:
+                    self.location = self.location.prev
+                    self.x += self.speed
+                    board.curr = board.curr.prev
+                    self.atCorner = False
+
+class Node:    
+    def __init__(self, x, y, orientation):
+        self.x = x
+        self.y = y
+        self.next = None
+        self.prev = None
+        self.orientation = orientation
+
+def chkBtwn(node1, node2, node3):
+    if node2.x == node1.x and node3.x == node2.x:
+        if node2.y > node1.y:
+            if node3.y >= node1.y and node3.y < node2.y:
+                return True
+        if node2.y < node1.y:
+            if node3.y > node2.y and node3.y <= node1.y:
+                return True
+
+    if node2.y == node1.y and node3.y == node2.y:
+        if node2.x > node1.x:
+            if node3.x >= node1.x and node3.x < node2.x:
+                return True
+        if node2.x < node1.x:
+            if node3.x > node2.x and node3.x <= node1.x:
+                return True
+    return False
+
+def findAreaList(listNodes):
+    sum1, sum2 = 0
+    for i in range(len(listNodes) - 1):
+        sum1 += listNodes[i].x * listNodes[i + 1].y
+        sum2 += listNodes[i].y * listNodes[i + 1].x
+    return (sum1 - sum2) / 2
+
+class Board:  
+    def __init__(self):    
+        startingNodes = [Node(10, 10, RIGHT), Node(500, 10, UP), Node(500, 500, LEFT), Node(10, 500, DOWN)]
+        self.curr = startingNodes[0]
+        self.curr.prev = startingNodes[3]
+        self.curr.next = startingNodes[1]
+        self.curr = self.curr.next
+        for x in range(2):
+            self.curr.next = startingNodes[x + 1]
+            self.curr.prev = startingNodes[x - 1]
+            self.curr = self.curr.next
+        self.curr.prev = startingNodes[2]
+        self.curr.next = startingNodes[0]
+        self.curr = self.curr.next
+        self.startingArea = self.getArea()
+        
+    def addPush(self, nodes):
+        reversedNodes = nodes.reverse()
+        current = self.curr
+        tempList = [nodes[0]]
+        while chkBtwn(current, current.next, nodes[-1]) is not True:
+            current = current.next
+            tempList.append(current)
+        tempList.extend(reversedNodes)
+        if findAreaList(tempList) <= self.getArea():
+            self.curr.next = nodes[0]
+            for x in range(1, len(nodes) - 1):
+                nodes[x].prev = nodes[x-1]
+                nodes[x].next = nodes[x+1]
+            nodes[-1].prev = nodes[-2]
+            nodes[-1].next = current.next
+            while self.curr is not nodes[-1]:
+                self.curr = self.curr.next
+        else:
+            for x in nodes:
+                x.orientation *= -1
+            self.curr.next.prev = nodes[0]
+            current.next = nodes[-1]
+            for x in range(1, len(nodes) - 1):
+                nodes[x].prev = nodes[x+1]
+                nodes[x].next = nodes[x-1]
+            nodes[-1].prev = current
+            nodes[-1].next = nodes[-2]
+            while self.curr is not nodes[-1]:
+                self.curr = self.curr.next
+
+    def getArea(self):
+        current = self.curr
+        firstNode = current
+        sum1 = 0
+        sum2 = 0
+        current = current.next
+        sum1 += firstNode.x * current.y
+        sum2 += firstNode.y * current.x
+        while current is not firstNode:
+            sum1 += current.x * current.next.y
+            sum2 += current.y * current.next.x
+            current = current.next
+        return (sum1 - sum2) / 2
+
+    def checkWin(self, percent):
+        if self.getArea() <= ((percent / 100) * self.startingArea):
+            return True
+        else:
+            return False
+
+board = Board()
+player = Player(100, 10, board)
 while running:
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -193,26 +308,18 @@ while running:
     keys = pygame.key.get_pressed()
 
     direction = None
-    if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and x > 5:
-        x -= speed
-        direction = 4
-    elif (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and x < 505:
-        x += speed
-        direction = 1
-    elif (keys[pygame.K_UP] or keys[pygame.K_w]) and y > 5:
-        y -= speed
-        direction = 3
-    elif (keys[pygame.K_DOWN] or keys[pygame.K_s]) and y < 505:
-        y += speed
-        direction = 0
+    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        player.move(LEFT, board)
+    elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        player.move(RIGHT, board)
+    elif keys[pygame.K_UP] or keys[pygame.K_w]:
+        player.move(UP, board)
+    elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
+        player.move(DOWN, board)
 
     '''screen = pygame.Surface.copy(screen)'''
     clock.tick(60)
     screen.fill(AQUA)
     screen.blit(playerSurf, (0, 0))
-    player(direction)
-    board.drawBoard()
-    sparx.move(board)
-    sparx.draw()
     pygame.display.update()
 
